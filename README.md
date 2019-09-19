c32ec8eea209577ea90fde76e11a4923d3c38c4d
jaeyaya <45762003+jaeyaya@users.noreply.github.com>
2019-09-19 17:44:17 +0900

    db connect #1

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 #WeTube_green
 
 Cloning Youtube with VanilaJs and NodeJS
+
+##pages:
+
+-[ ] Home
+-[x] Join
+-[x] Login
+-[x] Search
+-[ ] User Detail
+-[ ] Edit Profile
+-[ ] Change Password
+-[ ] Upload
+-[ ] Video Detail
+-[ ] Edit Video
+
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,31 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+import {videos} from "../db.js";
+import routes from "../routes";
+export const home = (req, res) =>{
+
+res.render("home", { pageTitle: "Home", videos : videos})
+};
+export const search = (req, res) => {
+    const {query: { term: searchingFor }} = req;
+    // const searchingFor = req.query.term;
+    res.render("search", { pageTitle : "Search", searchingFor, videos});
+};
+export const getUpload = (req, res) => 
+res.render("upload", { pageTitle : "Upload"});
+
+export const postUpload = (req, res) => {
+    const {
+        body:{file, title, description}
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(1231))
+};
+
+export const videoDetail = (req, res) => 
+res.render("videoDetail", { pageTitle : "Video Detail"});
+
+export const editVideo = (req, res) => 
+res.render("editVideo", { pageTitle : "Edit Video"});
+
+export const deleteVideo = (req, res) =>
+ res.render("deleteVideo", { pageTitle : "Delete Video"});
+ 
